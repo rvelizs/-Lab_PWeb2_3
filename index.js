2c9b8c16c6ea5591882191f207a1e063f9e29cfa
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-20 09:47:02 -0500

    configuración index.js

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const http = require('http');
 const path = require('path');
 const express = require('express');
 const app = express();
 
-const server = http.createServer((request, response) => {
-	console.log(request.url);
-	response.end("Hola mundo");
-});
-server.listen(3000);
-console.log("Escuchando en: http://localhost:3000")
-
-
+app.use(express.static('pub'));
 
 app.listen(3000, () => {
 	console.log("Escuchando en: http://localhost:3000")
@@ -18,6 +10,6 @@ app.listen(3000, () => {
 });
 
 app.get('/', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'index.html'));
+	response.sendFile(path.resolve(__dirname, '/pub/index.html'));
 });
 
